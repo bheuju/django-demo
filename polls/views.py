5db7aa6ca8c8ae3659a6e08373f4880f22cfbed6
Bishal Heuju <b.heuju@gmail.com>
2025-04-08 10:00:11 +0545

    ignore pycache, add views

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def user_list(request):
+    users = User.objects.all()
+    user_list_str = ", ".join([user.username for user in users])
+    return HttpResponse(f"List of users: {user_list_str}")
